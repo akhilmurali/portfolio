f7184a0ebe6078915f896b1d45db521ef27bc33b
Akhil Murali <github@akhilmurali.com>
2018-09-03 16:18:11 +0530

    Optimize for mobile screen

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,3 @@
 $(function(){
     console.log('Jquery Initialized');
-    $(window).resize(function(){
-        if($(window).width() <= 700){
-            //Mobile:
-            console.log('Mobile');
-            $('.w-100').removeClass('.m-t-40');
-        }
-    });
 });
